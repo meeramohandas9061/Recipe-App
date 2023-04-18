15393308434785f6b4bf875e4618b762e1ba3616
Meera Mohandas <meera.mohandas@perfomatix.com>
2023-04-18 16:59:26 +0530

    flatlist issues - in progress

--- src/Screens/RecipeDetail.js
+++ src/Screens/RecipeDetail.js
@@ -47,13 +47,18 @@ const RecipeDetail = ({ navigation }) => {
       .then((response) => response.json())
       .then((responseJson) => {
         setComments(responseJson);
-        console.log("error dataaaaa", responseJson);
+        console.log("error dataaaaa........", responseJson);
       })
       .catch((error) => {
         alert(error);
       });
   };
 
+  const handleOnPress = (id, commenterName, comment) => {
+    postComment(id, commenterName, comment);
+    getComments(id);
+  };
+  useEffect(() => {}, [comments]);
   useEffect(() => {
     getRecipeDetail();
     getComments(id);
@@ -61,145 +66,157 @@ const RecipeDetail = ({ navigation }) => {
 
   console.log(singleRecipeDetail);
   return (
-    <SafeAreaView style={{ backgroundColor: "#ffff", flex: 1 }}>
-      <ScrollView>
-        <Text style={styles.recipeName}> {singleRecipeDetail.strMeal}</Text>
-        <Image
-          style={styles.image}
-          source={{ uri: singleRecipeDetail.strMealThumb }}
-        />
-        <Text style={styles.ingredientTitle}>Main Ingredients: </Text>
-        <Text style={styles.ingredents}>
-          {"\u2022"} {singleRecipeDetail.strIngredient1}:{" "}
-          {singleRecipeDetail.strMeasure1}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient2}:{" "}
-          {singleRecipeDetail.strMeasure2}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient3}:{" "}
-          {singleRecipeDetail.strMeasure3}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient4}:{" "}
-          {singleRecipeDetail.strMeasure4}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient5}:{" "}
-          {singleRecipeDetail.strMeasure5}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient6}:{" "}
-          {singleRecipeDetail.strMeasure6}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient7}:{" "}
-          {singleRecipeDetail.strMeasure7}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient8}:{" "}
-          {singleRecipeDetail.strMeasure8}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient9}:{" "}
-          {singleRecipeDetail.strMeasure9}
-          {"\n"}
-          {"\u2022"} {singleRecipeDetail.strIngredient10}:
-          {singleRecipeDetail.strMeasure10}
-        </Text>
-        <Text style={styles.ingredientTitle}>Steps to follow:</Text>
-        <Text style={styles.recipeInstructionText}>
-          {singleRecipeDetail.strInstructions}
-        </Text>
-        {singleRecipeDetail.strTags ? (
-          <View>
-            <Text style={styles.ingredientTitle}>Tags: </Text>
-            {/* put sapce in between commas */}
-            <Text style={styles.ingredents}>
-              {singleRecipeDetail.strTags.split(",").join(", ")}{" "}
+    // <SafeAreaView style={{ backgroundColor: "#ffff", flex: 1 }}>
+    <FlatList
+      data={comments}
+      // horizontal={true}
+      keyExtractor={(results) => results.comment}
+      renderItem={({ item }) => {
+        return (
+          <View style={styles.commentContainer}>
+            <View style={styles.commentsList}>
+              <Text style={styles.commentUser}>{item.username}:</Text>
+              <Text style={styles.CommentContent}>{item.comment}</Text>
+              <Text style={styles.CommnetCreationDate}>
+                {item.creation_date}
+              </Text>
+            </View>
+          </View>
+        );
+      }}
+      ListHeaderComponent={() => (
+        <>
+          <Text style={styles.recipeName}> {singleRecipeDetail.strMeal}</Text>
+          <Image
+            style={styles.image}
+            source={{ uri: singleRecipeDetail.strMealThumb }}
+          />
+          <Text style={styles.ingredientTitle}>Main Ingredients: </Text>
+          <Text style={styles.ingredents}>
+            {"\u2022"} {singleRecipeDetail.strIngredient1}:{" "}
+            {singleRecipeDetail.strMeasure1}
+            {"\n"}
+            {"\u2022"} {singleRecipeDetail.strIngredient2}:{" "}
+            {singleRecipeDetail.strMeasure2}
+            {"\n"}
+            {"\u2022"} {singleRecipeDetail.strIngredient3}:{" "}
+            {singleRecipeDetail.strMeasure3}
+            {"\n"}
+            {"\u2022"} {singleRecipeDetail.strIngredient4}:{" "}
+            {singleRecipeDetail.strMeasure4}
+            {"\n"}
+            {"\u2022"} {singleRecipeDetail.strIngredient5}:{" "}
+            {singleRecipeDetail.strMeasure5}
+            {"\n"}
+            {"\u2022"} {singleRecipeDetail.strIngredient6}:{" "}
+            {singleRecipeDetail.strMeasure6}
+          </Text>
+          <Text style={styles.ingredientTitle}>Steps to follow:</Text>
+          <Text style={styles.recipeInstructionText}>
+            {singleRecipeDetail.strInstructions}
+          </Text>
+          {singleRecipeDetail.strTags ? (
+            <View>
+              <Text style={styles.ingredientTitle}>Tags: </Text>
+              {/* put sapce in between commas */}
+              <Text style={styles.ingredents}>
+                {singleRecipeDetail.strTags.split(",").join(", ")}{" "}
+              </Text>
+            </View>
+          ) : null}
+          <View style={styles.bottomItems}>
+            {/* <TouchableOpacity
+    style={styles.saveButton}
+    onPress={() => {
+      if (isDisabled == true) {
+        setDisable(false);
+      } else {
+        setDisable(true);
+      }
+    }}
+  >
+    {isDisabled ? (
+      <Image
+        style={styles.saveImage}
+        source={require("../Images/save.png")}
+      />
+    ) : (
+      <Image
+        style={styles.saveImage}
+        source={require("../Images/saved.png")}
+      />
+    )}
+  </TouchableOpacity> */}
+            <TouchableOpacity
+              style={styles.startCook}
+              onPress={() =>
+                navigation.navigate("WebViewScreen", {
+                  urlLink: singleRecipeDetail.strYoutube,
+                })
+              }
+              disabled={singleRecipeDetail.strYoutube ? false : true}
+              // activeOpacity={singleRecipeDetail.strYoutube ? 1 : 0.5}
+            >
+              <Text style={styles.StartCookText}>View Cooking Video</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.commentView}>
+            {comments && comments.length ? (
+              <Text style={styles.commentsTitle}>
+                Comments({comments.length})
+              </Text>
+            ) : (
+              <Text style={styles.commentsTitle}>Comments(0)</Text>
+            )}
+
+            {/* <FlatList
+    data={comments}
+    // horizontal={true}
+    keyExtractor={(results) => results.comment}
+    renderItem={({ item }) => {
+      return (
+        <View style={styles.commentContainer}>
+          <View style={styles.commentsList}>
+            <Text style={styles.commentUser}>{item.username}:</Text>
+            <Text style={styles.CommentContent}>{item.comment}</Text>
+            <Text style={styles.CommnetCreationDate}>
+              {item.creation_date}
             </Text>
           </View>
-        ) : null}
-        <View style={styles.bottomItems}>
-          {/* <TouchableOpacity
-            style={styles.saveButton}
-            onPress={() => {
-              if (isDisabled == true) {
-                setDisable(false);
-              } else {
-                setDisable(true);
-              }
-            }}
-          >
-            {isDisabled ? (
-              <Image
-                style={styles.saveImage}
-                source={require("../Images/save.png")}
-              />
-            ) : (
-              <Image
-                style={styles.saveImage}
-                source={require("../Images/saved.png")}
-              />
-            )}
-          </TouchableOpacity> */}
-          <TouchableOpacity
-            style={styles.startCook}
-            onPress={() =>
-              navigation.navigate("WebViewScreen", {
-                urlLink: singleRecipeDetail.strYoutube,
-              })
-            }
-            disabled={singleRecipeDetail.strYoutube ? false : true}
-            // activeOpacity={singleRecipeDetail.strYoutube ? 1 : 0.5}
-          >
-            <Text style={styles.StartCookText}>View Cooking Video</Text>
-          </TouchableOpacity>
         </View>
-        <View style={styles.commentView}>
-          {comments && comments.length ? (
-            <Text style={styles.commentsTitle}>
-              Comments({comments.length})
-            </Text>
-          ) : (
-            <Text style={styles.commentsTitle}>Comments(0)</Text>
-          )}
-
-          <FlatList
-            data={comments}
-            // horizontal={true}
-            keyExtractor={(results) => results.comment}
-            renderItem={({ item }) => {
-              return (
-                <View style={styles.commentContainer}>
-                  <View style={styles.commentsList}>
-                    <Text style={styles.commentUser}>{item.username}:</Text>
-                    <Text style={styles.CommentContent}>{item.comment}</Text>
-                    <Text style={styles.CommnetCreationDate}>
-                      {item.creation_date}
-                    </Text>
-                  </View>
-                </View>
-              );
-            }}
-          />
-        </View>
-        <View style={styles.addCommentsConatiner}>
-          <Text style={styles.AddNewCommentTitle}>Add New Comment</Text>
-          <TextInput
-            style={styles.commenterName}
-            placeholder="Your Name"
-            value={commenterName}
-            onChangeText={setCommenterName}
-          />
-          <TextInput
-            style={styles.enterComment}
-            placeholder="Your Insights"
-            value={comment}
-            onChangeText={setComment}
-          />
-          <TouchableOpacity
-            style={styles.startCook}
-            onPress={() => postComment(id, commenterName, comment)}
-          >
-            <Text style={styles.StartCookText}>Submit</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      );
+    }}
+  /> */}
+          </View>
+        </>
+      )}
+      ListFooterComponent={() => (
+        <>
+          <View style={styles.addCommentsConatiner}>
+            <Text style={styles.AddNewCommentTitle}>Add New Comment</Text>
+            <TextInput
+              style={styles.commenterName}
+              placeholder="Your Name"
+              value={commenterName}
+              onChangeText={setCommenterName}
+            />
+            <TextInput
+              style={styles.enterComment}
+              placeholder="Your Insights"
+              value={comment}
+              onChangeText={setComment}
+            />
+            <TouchableOpacity
+              style={styles.startCook}
+              onPress={() => handleOnPress(id, commenterName, comment)}
+            >
+              <Text style={styles.StartCookText}>Submit</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
+    />
+    // </SafeAreaView>
   );
 };
 
